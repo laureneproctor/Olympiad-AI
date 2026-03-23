8f22158aa169535c9e2b3b74fef62fcebf86a34b
AntonioTagliatti <145154218+AntonioTagliatti@users.noreply.github.com>
2026-03-23 06:36:17 -0400

    help comment

--- aimo3/scripts/helpers.py
+++ aimo3/scripts/helpers.py
@@ -3,14 +3,19 @@ from collections import Counter
 from pathlib import Path
 import torch
 import yaml
+"""
+Helper functions for the AIMO3 project.
 
+1. get_repo_root: Get the root directory of the repository.
+2. get_config_path: Get the path to a YAML configuration file.
+3. load_yaml: Load a YAML file and return its contents as a dictionary.
+4. set_seeds: Set random seeds for reproducibility across random, torch, and CUDA (if available).
+"""
 def get_repo_root() -> Path:
     return Path(__file__).resolve().parents[1]
 
-
 def get_config_path(yaml_file: str):
     return get_repo_root() / "configs" / yaml_file
-
 
 def load_yaml(config_path) -> dict:
     config_path = Path(config_path)
